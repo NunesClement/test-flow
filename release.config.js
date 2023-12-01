64ae57ae59dbfb8c5ef93cc7d2535d7e36ab4c9b
NunesClement <nunes.clement19@gmail.com>
2023-12-01 16:59:26 +0100

    fix: new config

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config = {
-    branches: ['release'],
+    branches: ['release', {name: 'nightly', prerelease: true}],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
