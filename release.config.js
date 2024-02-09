5363ed17f9ed75221cbc9e336baafc71bd3a8848
NunesClement <nunes.clement19@gmail.com>
2024-02-09 17:15:52 +0100

    fix(app): standard

--- release.config.js
+++ release.config.js
@@ -1,3 +1,7 @@
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const branch = process.env.GITHUB_BASE_REF
+
+
 const config = {
     branches: ['release', {name: 'nightly', prerelease: true}],
     plugins: [
@@ -21,5 +25,22 @@ const config = {
         "@semantic-release/github"
       ]
 };
+
+if (config.branches.some((it) => it === branch || (it.name === branch && !it.prerelease))) {
+  config.plugins.push('@semantic-release/changelog', [
+    '@semantic-release/git',
+    {
+      assets: [
+        'CHANGELOG.md',
+        'package.json',
+        'package-lock.json',
+        'packages/**/package.json',
+        'apps/**/package.json',
+      ],
+      // eslint-disable-next-line no-template-curly-in-string
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
+  ])
+}
 
 module.exports = config;
