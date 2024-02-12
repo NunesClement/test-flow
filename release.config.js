7fffbf107117fd06c521aa7eb81fac62346f5ed0
NunesClement <nunes.clement19@gmail.com>
2024-02-12 16:34:02 +0100

    fix: test

--- release.config.js
+++ release.config.js
@@ -3,22 +3,22 @@ const branch = process.env.GITHUB_REF_NAME // URG: check without git env
 console.log({ branch })
 const pluginsRelease = [
         "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
-        "@semantic-release/npm",
-        [
-          "@semantic-release/git",
-          {
-            "assets": [
-              "CHANGELOG.md",
-              "package.json",
-              "package-lock.json",
-              "packages/**/package.json",
-            ],
-            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-          }
-        ],
-        "@semantic-release/github",
+        // "@semantic-release/release-notes-generator",
+        // "@semantic-release/changelog",
+        // "@semantic-release/npm",
+        // [
+        //   "@semantic-release/git",
+        //   {
+        //     "assets": [
+        //       "CHANGELOG.md",
+        //       "package.json",
+        //       "package-lock.json",
+        //       "packages/**/package.json",
+        //     ],
+        //     "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        //   }
+        // ],
+        // "@semantic-release/github",
  ]
 
 
